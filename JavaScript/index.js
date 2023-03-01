96ba01310ec393476f49cdfd44d723d84c67e7b0
Daniel Ting <harmeyforever5435@gmail.com>
2023-02-28 22:09:36 -0500

    Lots of css edits to home page

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,31 +1,10 @@
-// Function to highlight the current section in the navigation menu
-function highlightCurrentSection() {
-  const sections = document.querySelectorAll("main section");
-  const navLinks = document.querySelectorAll("nav a");
-
-  sections.forEach((section, index) => {
-    const sectionTop = section.offsetTop - 50;
-    const sectionBottom = sectionTop + section.offsetHeight;
-
-    if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
-      navLinks.forEach((navLink) => {
-        navLink.classList.remove("active");
-      });
-      navLinks[index].classList.add("active");
-    }
-  });
-}
-
-// Event listener to call the highlightCurrentSection function on scroll
-window.addEventListener("scroll", highlightCurrentSection);
-
 // Function to toggle the mobile navigation menu
 function toggleMobileMenu() {
-  const navLinks = document.querySelector("nav ul");
+  const navLinks = document.querySelector("li a");
 
   navLinks.classList.toggle("show");
 }
 
 // Event listener to call the toggleMobileMenu function on click of the mobile menu button
-const menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", toggleMobileMenu);
+const menuButton = document.getElementsByClassName("menu");
+menuButton.addEventListener("click", toggleMobileMenu());
