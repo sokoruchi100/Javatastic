470b468808880ea3a76b12c0213ae5fbe7f296a5
Daniel Ting <harmeyforever5435@gmail.com>
2023-03-10 14:33:38 -0500

    Edited file structure

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,6 +1,7 @@
 "use strict"
 $(document).ready(function() {
   $("#menuButton").click(function(){
+    //For Hamburger Button
     if ($("#menuButton").hasClass("collapsed")) {
       $(".navbar-toggler-icon").removeClass("navOpen");
       $("#menuButton").css("border", "2px outset black");
@@ -8,5 +9,10 @@ $(document).ready(function() {
       $(".navbar-toggler-icon").addClass("navOpen");
       $("#menuButton").css("border", "2px inset black");
     }
+    $(".secondaryCollapse").collapse("hide");
+  })
+
+  $(".nav-link").click(function(){
+    $(".secondaryCollapse").collapse("hide");
   })
 });
