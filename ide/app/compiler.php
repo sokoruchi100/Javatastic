fdf0b1a50b0072f89fb782aa25fc77184d7c53f8
Daniel Ting <harmeyforever5435@gmail.com>
2023-03-12 21:33:38 -0400

    IDE testing

---
+++ ide/app/compiler.php
@@ -0,0 +1,32 @@
+<?php
+    //Stores code
+    $code = $_POST['code'];
+    //Name of class depends on exercise, posted by javascript
+    $className = "Main";
+
+    //Delete all old temp files
+    //get all file names from temp
+    $files = glob('temp/*');
+    //iterate files
+    foreach($files as $file){ 
+        if(is_file($file)) {
+            // delete file
+            unlink($file); 
+        }
+    }
+    //Creates the path for java file
+    $filePath = "temp/" . $className . ".java";
+    //creates a file in the temp folder
+    $programFile = fopen($filePath, "w");
+    //Writes code into file
+    fwrite($programFile, $code);
+    //Closes file
+    fclose($programFile);
+
+    //Creates output from runninng java compiler on file
+    shell_exec("compilers\javac.exe $filePath");
+    $output = shell_exec("cd temp && ..\compilers\java.exe $className 2>&1");
+    //Returns output to javascript
+    echo $output;
+    
+?>
---
+++ ide/ui/js/ide.js
@@ -0,0 +1,25 @@
+"use strict";
+let editor;
+
+window.onload = function() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/java");
+}
+
+function executeCode() {
+
+    $.ajax({
+        url: "/ide/app/compiler.php",
+
+        method: "POST",
+
+        data: {
+            code: editor.getSession().getValue()
+        },
+
+        success: function(response) {
+            $(".output").text(response)
+        }
+    })
+}
